6291e8980f9e496a88f53888436bb1c4b6294f9a
aerobounce <10491362+aerobounce@users.noreply.github.com>
2020-01-09 12:10:48 +0900

    pep8 format conform to pylint use tempfile module for compatibility refactor shfmt command composing for readability remove unused commented lines

--- PrettyShell.py
+++ PrettyShell.py
@@ -8,38 +8,32 @@
 # Copyright © 2019 aerobounce. All rights reserved.
 #
 
+from subprocess import Popen, PIPE
+import tempfile
 import sublime
 import sublime_plugin
-from uuid import uuid4
-from subprocess import Popen, PIPE
-from os import remove
+
 
 class PrettyShellCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        settings=sublime.load_settings("Pretty Shell.sublime-settings")
+        # Load settings
+        settings = sublime.load_settings("Pretty Shell.sublime-settings")
 
-        # Store cursor region
-        # original_regions = self.view.sel()
-
-        # Prepare formatted result
-        # If has no selection, entire regions will be used
-        # for region in self.view.sel():
-        #     if region.empty():
+        # Read current file
         selection = sublime.Region(0, self.view.size())
-            # else:
-            #     selection = region
         target_text = self.view.substr(selection)
 
         # Make temp file
-        tmp_file_path = "/tmp/" + str(uuid4())
+        tmp_file = tempfile.NamedTemporaryFile()
+        tmp_file_path = tmp_file.name
 
         # Write texts
-        with open(tmp_file_path, mode='w', encoding='utf-8') as tmp:
+        with open(tmp_file_path, mode="w", encoding="utf-8") as tmp:
             tmp.write(target_text)
 
-        # Format temp file with shfmt
-        indent = str(settings.get("indent"))
-        language = str(settings.get("language"))
+        # Compose shfmt command
+        indent = "-i {0}".format(str(settings.get("indent")))
+        language = "-ln \"{0}\"".format(str(settings.get("language")))
         simplify = "-s" if settings.get("simplify", True) else ""
         binop = "-bn" if settings.get("binop", True) else ""
         switchcase = "-ci" if settings.get("switchcase", True) else ""
@@ -47,40 +41,31 @@ class PrettyShellCommand(sublime_plugin.TextCommand):
         align = "-kp" if settings.get("align", True) else ""
         minify = "-mn" if settings.get("minify", True) else ""
 
-        command = "shfmt -w -i {1} -ln \"{2}\" {3} {4} {5} {6} {7} {8} \"{0}\"".format(tmp_file_path,
-                                                                                       indent,
-                                                                                       language,
-                                                                                       simplify,
-                                                                                       binop,
-                                                                                       switchcase,
-                                                                                       rediop,
-                                                                                       align,
-                                                                                       minify)
-        # print(command)
+        command = "shfmt -w \"{0}\" ".format(tmp_file_path)
+        command += "{0} ".format(indent)
+        command += "{0} ".format(language)
+        command += "{0} ".format(simplify)
+        command += "{0} ".format(binop)
+        command += "{0} ".format(switchcase)
+        command += "{0} ".format(rediop)
+        command += "{0} ".format(align)
+        command += "{0}".format(minify)
+
+        # Format
+        print(command)
         Popen(command, shell=True, stdout=PIPE).stdout.read()
 
         # Read result
-        with open(tmp_file_path, encoding='utf-8') as temp:
+        with open(tmp_file_path, encoding="utf-8") as temp:
             output = temp.read()
-
-        # Remove temp file
-        remove(tmp_file_path)
 
         # Replace with result
         self.view.replace(edit, selection, output)
 
-        # Restore cursor region
-        # try:
-        #     selection = original_regions[0]
-        #     self.view.sel().clear()
-        #     self.view.sel().add(selection)
-        # except IndexError:
-        #     return
-
 
 class AutoFormatter(sublime_plugin.ViewEventListener):
     def on_pre_save(self):
-        settings=sublime.load_settings("Pretty Shell.sublime-settings")
-        if settings.get("pretty_on_save") == True:
+        settings = sublime.load_settings("Pretty Shell.sublime-settings")
+        if settings.get("pretty_on_save", False):
             if "Bash" in self.view.settings().get("syntax"):
                 self.view.run_command("pretty_shell")
