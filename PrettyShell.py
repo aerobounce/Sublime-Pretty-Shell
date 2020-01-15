68aacc7b61726adacb26c765e0c7df2156af3ff9
aerobounce <10491362+aerobounce@users.noreply.github.com>
2020-01-15 20:05:52 +0900

    some refactorings and bug fix revert class var settings simplify command composing use memory instead of creating temp file to format print error to console when needed

--- PrettyShell.py
+++ PrettyShell.py
@@ -8,31 +8,22 @@
 # Copyright © 2019 aerobounce. All rights reserved.
 #
 
-import tempfile
 from subprocess import PIPE, Popen
 
 import sublime
 import sublime_plugin
 
+SETTINGS_FILENAME = "Pretty Shell.sublime-settings"
+
 
 class PrettyShellCommand(sublime_plugin.TextCommand):
-
-    settings = None
-
     def run(self, edit):
-        # Make temp file
-        tmp_file = tempfile.NamedTemporaryFile()
-        tmp_file_path = tmp_file.name
-
-        # Read current file and write to temp file
+        # Read current file
         selection = sublime.Region(0, self.view.size())
         target_text = self.view.substr(selection)
 
-        with open(tmp_file_path, mode="w", encoding="utf-8") as tmp:
-            tmp.write(target_text)
-
         # Retrieve settings
-        settings = PrettyShellCommand.settings
+        settings = sublime.load_settings(SETTINGS_FILENAME)
 
         shfmt_bin_path = "{0} ".format(settings.get("shfmt_bin_path"))
         simplify = "-s " if settings.get("simplify") else ""
@@ -42,39 +33,39 @@ class PrettyShellCommand(sublime_plugin.TextCommand):
         switchcase = "-ci " if settings.get("switchcase") else ""
         rediop = "-sr " if settings.get("rediop") else ""
         align = "-kp " if settings.get("align") else ""
-        minify = "-mn " if settings.get("minify") else ""
+        minify = "-mn" if settings.get("minify") else ""
 
         # Compose shfmt command
-        command = shfmt_bin_path
-        command += simplify
-        command += language
-        command += indent
-        command += binop
-        command += switchcase
-        command += rediop
-        command += align
-        command += minify
-        command += '-w "{0}"'.format(tmp_file_path)
+        command = (
+            shfmt_bin_path
+            + simplify
+            + language
+            + indent
+            + binop
+            + switchcase
+            + rediop
+            + align
+            + minify
+        )
 
-        # Format
-        print(command)
-        Popen(command, shell=True, stdout=PIPE).stdout.read()
-        # print(Popen(command, shell=True, stderr=PIPE).stderr.read())
+        # Format and Read result
+        with Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE) as popen:
+            popen.stdin.write(target_text.encode("utf-8"))
+            popen.stdin.close()
+            output = popen.stdout.read().decode("utf-8")
 
-        # Read result
-        with open(tmp_file_path, encoding="utf-8") as tmp:
-            output = tmp.read()
+            # Replace result if only exit code is 0
+            if popen.wait() == 0:
+                # Replace with result
+                self.view.replace(edit, selection, output)
 
-        # Replace with result
-        self.view.replace(edit, selection, output)
+            # Print error message
+            else:
+                print("Pretty Shell error:", popen.stderr.read().decode("utf-8"))
 
 
 class AutoFormatter(sublime_plugin.ViewEventListener):
     def on_pre_save(self):
-        PrettyShellCommand.settings = sublime.load_settings(
-            "Pretty Shell.sublime-settings"
-        )
-
-        if PrettyShellCommand.settings.get("pretty_on_save", False):
-            if "Bash" in self.view.settings().get("syntax"):
+        if "Bash" in self.view.settings().get("syntax"):
+            if sublime.load_settings(SETTINGS_FILENAME).get("pretty_on_save"):
                 self.view.run_command("pretty_shell")
