d87e24217df08bca0cf91273919a7688fc953507
aerobounce <10491362+aerobounce@users.noreply.github.com>
2020-03-05 15:06:39 +0900

    add new commands and option, some tweak for output panel
    new commands:
    "pretty_shell_selection"
    "pretty_shell_minify"
    "pretty_shell_minify_selection"
    
    new option:
    "format_selection_only"
    
    renamed option:
    "pretty_on_save" -> "format_on_save"
    
    updates for output panel:
    use base name of opened file path (use buffer id for untitled buffers)
    use delay to show panel to make it front
    no longer using Bash syntax

--- PrettyShell.py
+++ PrettyShell.py
@@ -8,6 +8,7 @@
 # Copyright © 2019 aerobounce. All rights reserved.
 #
 
+from os import path
 from subprocess import PIPE, Popen
 
 import sublime
@@ -17,39 +18,36 @@ SETTINGS_FILENAME = "Pretty Shell.sublime-settings"
 OUTPUT_PANEL_NAME = "pretty_shell_error"
 
 
-class PrettyShellCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # Read whole of the current active file
-        selection = sublime.Region(0, self.view.size())
-        target_text = self.view.substr(selection)
+def invoke_formatter(view, edit, use_selection, minify):
+    # Load settings file
+    settings = sublime.load_settings(SETTINGS_FILENAME)
 
-        # Load settings file
-        settings = sublime.load_settings(SETTINGS_FILENAME)
+    # Retrieve settings (No need for nil fallback here)
+    shfmt_bin_path = "{0} ".format(settings.get("shfmt_bin_path"))
+    simplify = "-s " if settings.get("simplify") else ""
+    language = '-ln "{0}" '.format(settings.get("language"))
+    indent = "-i {0} ".format(settings.get("indent"))
+    binop = "-bn " if settings.get("binop") else ""
+    switchcase = "-ci " if settings.get("switchcase") else ""
+    rediop = "-sr " if settings.get("rediop") else ""
+    align = "-kp " if settings.get("align") else ""
+    minify = "-mn" if (settings.get("minify") or minify) else ""
 
-        # Retrieve settings (No need for nil fallback here)
-        shfmt_bin_path = "{0} ".format(settings.get("shfmt_bin_path"))
-        simplify = "-s " if settings.get("simplify") else ""
-        language = '-ln "{0}" '.format(settings.get("language"))
-        indent = "-i {0} ".format(settings.get("indent"))
-        binop = "-bn " if settings.get("binop") else ""
-        switchcase = "-ci " if settings.get("switchcase") else ""
-        rediop = "-sr " if settings.get("rediop") else ""
-        align = "-kp " if settings.get("align") else ""
-        minify = "-mn" if settings.get("minify") else ""
+    # Compose shfmt command
+    command = (
+        shfmt_bin_path
+        + simplify
+        + language
+        + indent
+        + binop
+        + switchcase
+        + rediop
+        + align
+        + minify
+    )
 
-        # Compose shfmt command
-        command = (
-            shfmt_bin_path
-            + simplify
-            + language
-            + indent
-            + binop
-            + switchcase
-            + rediop
-            + align
-            + minify
-        )
-
+    # Format
+    def invoke_command(target_text, selection):
         # Open subprocess with the command
         with Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE) as popen:
             # Write selection into stdin, then ensure the descriptor is closed
@@ -61,46 +59,99 @@ class PrettyShellCommand(sublime_plugin.TextCommand):
 
             # Replace with result if only stderr is empty
             if stderr == "":
-                self.view.replace(edit, selection, stdout)
+                view.replace(edit, selection, stdout)
 
             # Update output panel state
-            self.manage_output_panel(edit, stderr)
+            manage_output_panel(view, edit, stderr)
 
-    def manage_output_panel(self, edit, stderr):
-        # Remove output panel if stderr is empty
-        if stderr == "":
-            self.view.window().destroy_output_panel(OUTPUT_PANEL_NAME)
-            return
+    def format_entire_file():
+        if not use_selection:
+            selection = sublime.Region(0, view.size())
+            target_text = view.substr(selection)
+            invoke_command(target_text, selection)
 
-        # Otherwise update output panel
-        panel = self.view.window().find_output_panel(OUTPUT_PANEL_NAME)
+    # Prevent needles iteration AMAP
+    if settings.get("format_selection_only") or use_selection:
+        if any([not r.empty() for r in view.sel()]):
+            for region in view.sel():
+                if not region.empty():
+                    target_text = view.substr(region)
+                    invoke_command(target_text, region)
 
-        # Initialize output panel if `panel` is `None`
-        if not panel:
-            panel = self.view.window().create_output_panel(OUTPUT_PANEL_NAME)
-            panel.settings().set("draw_centered", True)
-            panel.settings().set("syntax", "Packages/ShellScript/Bash.sublime-syntax")
+        else:
+            format_entire_file()
 
-        # Update output panel contents
-        stderr = "Pretty Shell - Error:\n" + stderr
-        panel.set_read_only(False)
-        # Replace with stderr strings
-        panel.replace(edit, sublime.Region(0, panel.size()), stderr)
-        panel.set_viewport_position((0, 0), False)
-        # Clear selections
-        panel.sel().clear()
-        # Prepare panel size
-        panel.show(panel.size() - 1)
-        panel.set_read_only(True)
+    else:
+        format_entire_file()
 
-        # Show output panel
-        self.view.window().run_command(
+
+def manage_output_panel(view, edit, stderr):
+    # Remove output panel if stderr is empty
+    if stderr == "":
+        view.window().destroy_output_panel(OUTPUT_PANEL_NAME)
+        return
+
+    # Otherwise update output panel
+    panel = view.window().find_output_panel(OUTPUT_PANEL_NAME)
+
+    # Initialize output panel if `panel` is `None`
+    if not panel:
+        panel = view.window().create_output_panel(OUTPUT_PANEL_NAME)
+        panel.settings().set("draw_centered", True)
+        # panel.settings().set("syntax", "Packages/ShellScript/Bash.sublime-syntax")
+
+    # Update output panel contents
+    # Filename idea borrowed from SublimeLinter
+    file_name = (
+        path.basename(view.file_name())
+        if view.file_name()
+        else "untitled (Buffer ID {})".format(view.buffer_id())
+    )
+    # Format stderr
+    stderr = stderr.replace("<standard input>:", "    ")
+    message = file_name + "\n" + stderr
+    panel.set_read_only(False)
+    # Replace with stderr strings
+    panel.replace(edit, sublime.Region(0, panel.size()), message)
+    panel.set_viewport_position((0, 0), False)
+    # Clear selections
+    panel.sel().clear()
+    # Prepare panel size
+    panel.show(panel.size() - 1)
+    panel.set_read_only(True)
+
+    # Show output panel
+    # Dirty hack to prioritize this panel over SublimeLinter
+    sublime.set_timeout_async(
+        lambda: view.window().run_command(
             "show_panel", {"panel": "output.{0}".format(OUTPUT_PANEL_NAME)}
-        )
+        ),
+        160,
+    )
 
 
-class AutoFormatter(sublime_plugin.ViewEventListener):
+class PrettyShellCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        invoke_formatter(self.view, edit, False, False)
+
+
+class PrettyShellSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        invoke_formatter(self.view, edit, True, False)
+
+
+class PrettyShellMinifyCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        invoke_formatter(self.view, edit, False, True)
+
+
+class PrettyShellMinifySelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        invoke_formatter(self.view, edit, True, True)
+
+
+class PrettyShellListener(sublime_plugin.ViewEventListener):
     def on_pre_save(self):
         if "Bash" in self.view.settings().get("syntax"):
-            if sublime.load_settings(SETTINGS_FILENAME).get("pretty_on_save"):
+            if sublime.load_settings(SETTINGS_FILENAME).get("format_on_save"):
                 self.view.run_command("pretty_shell")
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # 🐚 Pretty Shell
 
-### Shell Script beautifier plugin for Sublime Text 3
+### Shell Script Beautifier/Minifier/SimpleLinter plugin for Sublime Text 3
 - [packagecontrol.io/packages/Pretty Shell](https://packagecontrol.io/packages/Pretty%20Shell)
-- Powered by [mvdan/sh](https://github.com/mvdan/sh), quality shell script formatter.
+- Powered by **[mvdan/sh](https://github.com/mvdan/sh)**, quality shell script formatter.
+- **Blazingly Fast.**
+- **Simple Linting.**
 - Recommended for shell scripting beginners.
     - I've written this plugin for myself to learn how to write shell scripts in correct syntax.
     - **Highly recommended to use this plugin with: [SublimeLinter-shellcheck](https://packagecontrol.io/packages/SublimeLinter-shellcheck)**
@@ -13,66 +15,83 @@
 3. You're ready
 
 ### 📦 Manual Install
-1. Quit Sublime Text
-2. Clone this repository as shown below
-3. You're ready
+1. Clone this repository as shown below
+2. You're ready (Restart Sublime Text if the package is not recognized)
 
 ```bash
-# Example on macOS 
+# Example on macOS
 git clone https://github.com/aerobounce/Sublime-Pretty-Shell.git "$HOME/Library/Application Support/Sublime Text 3/Packages/PrettyShell"
-# It should work on Linux / Windows too.
+# It should work on Linux / Windows too,
 # Follow the same steps with the equivalent clone target directory.
 ```
 
 ### ⚠️ Dependency
 **Pretty Shell** does not work without `shfmt` as this package utilizes the formatter.<br>
-`shfmt` is available via several package managers.<br>
+`shfmt` is available via several package managers and binary releases by its author.<br>
 
 - **macOS**
-    - [Homebrew](https://formulae.brew.sh/formula/shfmt), [MacPorts](https://ports.macports.org/port/shfmt/summary)
+    - [Homebrew](https://formulae.brew.sh/formula/shfmt)
+    - [MacPorts](https://ports.macports.org/port/shfmt/summary)
 - **\*nix**
-    - [Alpine](https://pkgs.alpinelinux.org/packages?name=shfmt), [Arch](https://www.archlinux.org/packages/community/x86_64/shfmt/), [FreeBSD](https://www.freshports.org/devel/shfmt), [NixOS](https://github.com/NixOS/nixpkgs/blob/HEAD/pkgs/tools/text/shfmt/default.nix), [Snapcraft](https://snapcraft.io/shfmt), [Void](https://github.com/void-linux/void-packages/blob/HEAD/srcpkgs/shfmt/template)
+    - [Linuxbrew](https://github.com/Homebrew/linuxbrew-core/blob/master/Formula/shfmt.rb)
+    - [Alpine](https://pkgs.alpinelinux.org/packages?name=shfmt)
+    - [Arch](https://www.archlinux.org/packages/community/x86_64/shfmt/)
+    - [FreeBSD](https://www.freshports.org/devel/shfmt)
+    - [NixOS](https://github.com/NixOS/nixpkgs/blob/HEAD/pkgs/tools/text/shfmt/default.nix)
+    - [Snapcraft](https://snapcraft.io/shfmt)
+    - [Void](https://github.com/void-linux/void-packages/blob/HEAD/srcpkgs/shfmt/template)
 - **Windows**
     - [Scoop](https://github.com/ScoopInstaller/Main/blob/HEAD/bucket/shfmt.json)
-- **Binary Releases**
+- **Pre-built binary releases**
     - [mvdan/sh/releases](https://github.com/mvdan/sh/releases)
 
 If installed via a package manager and **Sublime Text** recognizes its path, that's it.<br>
-Otherwise, an error will be shown. Specify absolute path to the executable in the settings:
+Otherwise, specify the absolute path in the settings:
 
 ```JavaScript
 "shfmt_bin_path": "Absolute Path to shfmt"
 ```
 
 ## ⌨️ Usage
-1. <kbd>Command Palette...</kbd>
-2. <kbd>Pretty Shell: Format</kbd>
 
-or just save script file you are editing while `pretty_on_save` is `true`.
+### 📝 Available Commands
+| Caption                                   | Command                         | Default Key Bindings                                              |
+| ----------------------------------------- | ------------------------------- | ----------------------------------------------------------------- |
+| <kbd>Pretty Shell: Format</kbd>           | `pretty_shell`                  | <kbd>cmd</kbd> or <kbd>alt</kbd> + <kbd>ctrl</kbd> + <kbd>s</kbd> |
+| <kbd>Pretty Shell: Format Selection</kbd> | `pretty_shell_selection`        | None                                                              |
+| <kbd>Pretty Shell: Minify</kbd>           | `pretty_shell_minify`           | None                                                              |
+| <kbd>Pretty Shell: Minify Selection</kbd> | `pretty_shell_minify_selection` | None                                                              |
 
-### 🔤 Default Key Bindings
-- macOS:   <kbd>cmd</kbd> + <kbd>ctrl</kbd> + <kbd>s</kbd>
-- Linux:   <kbd>alt</kbd> + <kbd>ctrl</kbd> + <kbd>s</kbd>
-- Windows: <kbd>alt</kbd> + <kbd>ctrl</kbd> + <kbd>s</kbd>
+- **Command** is the name of the command you can use for **Key-Bindings**.
+- Be aware that any manual modifications with `Format Selection` commands might be lost upon saving a file if `format_on_save` is `true`, which it is by default.
 
-### 🛠 Options
+### 🛠 Default Settings
 ```javascript
-"simplify": true, // Simplify the code
+// shfmt settings
+"simplify": true,   // Simplify the code
 "language": "bash", // Language variant to parse (bash / posix / mksh)
-"indent": 4, // 0 for tabs
-"binop": false, // Binary operators such as '&&' and '|' may start a line
+"indent": 4,        // 0 for tabs
+"binop": false,     // Binary operators such as '&&' and '|' may start a line
 "switchcase": true, // Indent switch cases
-"rediop": true, // Redirect operators will be followed by a space
-"align": false, // Keep column alignment paddings
-"minify": false, // Minify program to reduce its size
-"pretty_on_save": true,
+"rediop": true,     // Redirect operators will be followed by a space
+"align": false,     // Keep column alignment paddings
+"minify": false,    // Minify program to reduce its size
+
+// Pretty Shell settings
+"format_selection_only": false, // Entire file will be used if no selections
+"format_on_save": true,
 "shfmt_bin_path": "shfmt"
 ```
 
 ## ☑️ Todo
-- [x] Show parsing errors
-- [ ] Format only selection
-- [x] ~~Ship with `shfmt` binaries for portability?~~ -> Package will be too heavy
+- [ ] Highlight `shfmt`'s error position in `view` to use this package as simple shellscript linter
+- [ ] Make output panel's coloring smarter (like SublimeLinter)
+- [x] Make output panel always frontmost, as it's always fatal error `shfmt` cannot handle when it needs the panel
+- [x] Add `Format Entire File` command
+- [x] Add `Format Selection` command
+- [x] Add `Minify Entire File` command
+- [x] Add `Minify Selection` command
+- [x] Add `format_selection_only` option
 
 ## 🤝 Special Thanks
 - [dzhibas/SublimePrettyJson](https://github.com/dzhibas/SublimePrettyJson) (This project is inspired by the package)
