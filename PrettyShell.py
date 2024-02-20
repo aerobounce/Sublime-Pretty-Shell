580349e5bf5d4ba5cbe3ebd83ec8e96be1253b01
Jack Cherng <jfcherng@gmail.com>
2024-02-20 20:19:58 +0800

    ST API shouldn't be called until it's ready
    As of ST 4170, calling ST API before it's ready will raise an exception.
    And this causes this plugin to be totally ignored (and thus unusable).
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- PrettyShell.py
+++ PrettyShell.py
@@ -8,11 +8,12 @@
 
 from html import escape
 from re import compile
-from subprocess import PIPE, Popen
+from subprocess import PIPE
+from subprocess import Popen
 
-from sublime import LAYOUT_BELOW, Edit, Phantom, PhantomSet, Region, View
-from sublime import error_message as alert
-from sublime import load_settings, packages_path
+from sublime import LAYOUT_BELOW
+from sublime import Edit, Phantom, PhantomSet, Region, View
+from sublime import error_message as alert, load_settings, packages_path
 from sublime_plugin import TextCommand, ViewEventListener
 
 SETTINGS_FILENAME = "Pretty Shell.sublime-settings"
@@ -52,84 +53,66 @@ PHANTOM_STYLE = """
 """
 
 
-def plugin_loaded():
-    PrettyShell.settings = load_settings(SETTINGS_FILENAME)
-    PrettyShell.reload_settings()
-    PrettyShell.settings.add_on_change(ON_CHANGE_TAG, PrettyShell.reload_settings)
-
-
-def plugin_unloaded():
-    PrettyShell.settings.clear_on_change(ON_CHANGE_TAG)
+def get_plugin_settings():
+    return load_settings(SETTINGS_FILENAME)
 
 
 class PrettyShell:
-    settings = load_settings(SETTINGS_FILENAME)
     phantom_sets = {}
-    shell_command = ""
-    shell_cwd = ""
-    format_on_save = True
-    show_error_inline = True
-    scroll_to_error_point = True
 
-    @classmethod
-    def reload_settings(cls):
-        cls.format_on_save = cls.settings.get("format_on_save")
-        cls.show_error_inline = cls.settings.get("show_error_inline")
-        cls.scroll_to_error_point = cls.settings.get("scroll_to_error_point")
+    @staticmethod
+    def construct_shell_command():
+        settings = get_plugin_settings()
 
-        simplify = cls.settings.get("simplify")
-        minify = cls.settings.get("minify")
-        language = cls.settings.get("language")
-        indent = cls.settings.get("indent")
-        binop = cls.settings.get("binop")
-        switchcase = cls.settings.get("switchcase")
-        rediop = cls.settings.get("rediop")
-        align = cls.settings.get("align")
-        fnbrace = cls.settings.get("fnbrace")
+        simplify = settings.get("simplify")
+        minify = settings.get("minify")
+        language = settings.get("language")
+        indent = settings.get("indent")
+        binop = settings.get("binop")
+        switchcase = settings.get("switchcase")
+        rediop = settings.get("rediop")
+        align = settings.get("align")
+        fnbrace = settings.get("fnbrace")
 
-        cls.shell_command = cls.settings.get("shfmt_bin_path")
+        shell_command = settings.get("shfmt_bin_path")
         if simplify:
-            cls.shell_command += " -s"
+            shell_command += " -s"
         if minify:
-            cls.shell_command += " -mn"
+            shell_command += " -mn"
         if language:
-            cls.shell_command += ' -ln "{}"'.format(language)
+            shell_command += ' -ln "{}"'.format(language)
         if indent:
-            cls.shell_command += " -i {}".format(indent)
+            shell_command += " -i {}".format(indent)
         if binop:
-            cls.shell_command += " -bn"
+            shell_command += " -bn"
         if switchcase:
-            cls.shell_command += " -ci"
+            shell_command += " -ci"
         if rediop:
-            cls.shell_command += " -sr"
+            shell_command += " -sr"
         if align:
-            cls.shell_command += " -kp"
+            shell_command += " -kp"
         if fnbrace:
-            cls.shell_command += " -fn"
+            shell_command += " -fn"
 
-        # Note: For Windows only, UNC path error workaround.
-        # ("CMD does not support UNC paths as current directories.")
-        # This may not be needed in Sublime Text 4.
-        #
-        # Popen needs non UNC `cwd` to be specified.
-        # It seems `cwd` can be any path as long as it's a non UNC path
-        cls.shell_cwd = packages_path()
+        return shell_command
 
     @classmethod
     def update_phantoms(cls, view: View, stderr: str, error_point: int):
         view_id = view.id()
 
-        if not view_id in cls.phantom_sets:
+        if view_id not in cls.phantom_sets:
             cls.phantom_sets[view_id] = PhantomSet(view, str(view_id))
 
         # Create Phantom
         def phantom_content():
+            # Remove unneeded text from stderr
+            error_message = compile(r"[0-9]{1,}:[0-9]{1,}:.").sub("", stderr)
             return (
                 "<body id=inline-error>"
                 + PHANTOM_STYLE
                 + '<div class="error-arrow"></div><div class="error">'
                 + '<span class="message">'
-                + escape(stderr, quote=False)
+                + escape(error_message, quote=False)
                 + "</span>"
                 + "<a href=hide>"
                 + chr(0x00D7)
@@ -157,6 +140,17 @@ class PrettyShell:
 
     @classmethod
     def execute_format(cls, view: View, edit: Edit):
+        settings = get_plugin_settings()
+        shell_command = cls.construct_shell_command()
+
+        # Note: For Windows only, UNC path error workaround.
+        # ("CMD does not support UNC paths as current directories.")
+        # This may not be needed in Sublime Text 4.
+        #
+        # Popen needs non UNC `cwd` to be specified.
+        # It seems `cwd` can be any path as long as it's a non UNC path
+        shell_cwd = packages_path()
+
         # Get entire string
         entire_region = Region(0, view.size())
         entire_text = view.substr(entire_region)
@@ -166,7 +160,7 @@ class PrettyShell:
             return
 
         # Execute shell and get output
-        with Popen(cls.shell_command, cwd=cls.shell_cwd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE) as popen:
+        with Popen(shell_command, cwd=shell_cwd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE) as popen:
             # Nil check to suppress linter
             if not popen.stdin or not popen.stdout or not popen.stderr:
                 return
@@ -180,7 +174,7 @@ class PrettyShell:
             stderr = stderr.replace("\n", "")
 
         # Print command executed to the console
-        print("[Pretty Shell] Popen:", cls.shell_command)
+        print("[Pretty Shell] Popen:", shell_command)
 
         # Present alert for 'command not found'
         if "command not found" in stderr:
@@ -208,11 +202,11 @@ class PrettyShell:
 
         # Update Phantoms
         view.erase_phantoms(str(view.id()))
-        if cls.show_error_inline and error_point:
+        if settings.get("show_error_inline") and error_point:
             cls.update_phantoms(view, stderr, error_point)
 
         # Scroll to the syntax error point
-        if cls.scroll_to_error_point and error_point:
+        if settings.get("scroll_to_error_point") and error_point:
             view.sel().clear()
             view.sel().add(Region(error_point))
             view.show_at_center(error_point)
@@ -229,7 +223,9 @@ class PrettyShellCommand(TextCommand):
 
 class PrettyShellListener(ViewEventListener):
     def on_pre_save(self):
-        if PrettyShell.format_on_save:
+        settings = get_plugin_settings()
+
+        if settings.get("format_on_save"):
             if "Bash" in self.view.settings().get("syntax"):
                 self.view.run_command("pretty_shell")
 
